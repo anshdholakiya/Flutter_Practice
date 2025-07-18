af0b984f29bf5fd4cc22a8bbf46eeaae5c214ce7
Ansh Dholakiya <dholakiyaansh1707@gmail.com>
2025-07-18 08:43:52 +0530

    code updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,65 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromARGB(255, 255, 2, 2),
+          brightness: Brightness.dark,
+        ),
+      ),
+      home: LoginPage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return Scaffold(
+      // body: SafeArea(
+      //   child: Column(
+      //     mainAxisAlignment: MainAxisAlignment.center,
+      //     children: [
+      //       Text("Login", style: TextStyle(fontSize: 50, color: Colors.amber)),
+      //       Text("Username"),
+      //       TextField(maxLength: 5),
+      //       Text("Password"),
+      //       TextField(obscureText: true),
+      //       ElevatedButton(
+      //         onPressed: () {
+      //           print("this is print statement");
+      //         },
+      //         child: Text("Login"),
+      //       ),
+      //     ],
+      //   ),
+      // ),
+      body: Center(
+        child: Container(
+          height: double.infinity,
+          width: double.infinity,
+          margin: EdgeInsets.all(40),
+          padding: EdgeInsets.all(40),          
+          decoration: BoxDecoration(
+            color: Colors.red,
+            borderRadius: BorderRadius.circular(20),
+          ),                                     
+          
         ),
       ),
     );
